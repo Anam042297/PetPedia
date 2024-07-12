0bfc5a16f889d0e380ac3041efec892a1c9bcaba
Anam042297 <Anam042297@gmail.com>
2024-07-12 13:08:48 -0700

    awin

--- resources/views/dashboard/petcatagory/create.blade.php
+++ resources/views/dashboard/petcatagory/create.blade.php
@@ -10,7 +10,6 @@
         body {
             background: linear-gradient(to right, #F5F7FF, #F5F7FF);
             font-family: Arial, sans-serif;
-            background-image: url("Dashboard/images/dashboard/people2.png");
             height: 100%;
             background-position: right;
             background-repeat: no-repeat;
