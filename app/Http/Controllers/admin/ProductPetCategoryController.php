5d1524782ca869fc7b93a11bb66795163470e589
aishatabasum <ayesha.tabassum3210@gmail.com>
2024-08-20 07:07:23 -0700

    .

--- app/Http/Controllers/admin/ProductPetCategoryController.php
+++ app/Http/Controllers/admin/ProductPetCategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\PetCategory;
 use Yajra\DataTables\Facades\DataTables;
 
 class ProductPetCategoryController extends Controller
