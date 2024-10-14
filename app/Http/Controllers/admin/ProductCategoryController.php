791996303fcc34bb4d2d86d4765ffbe4161c6ec2
SaadUllah <bsaad955@gmail.com>
2024-10-14 20:12:38 +0500

    .

--- app/Http/Controllers/admin/ProductCategoryController.php
+++ app/Http/Controllers/admin/ProductCategoryController.php
@@ -16,16 +16,16 @@ class ProductCategoryController extends Controller
     {
         // dd($request);
         if ($request->ajax()) {
-            $data = ProductCategory::select('id', 'name','icon')
-            ->get();
-          //  dd($data);
+            $data = ProductCategory::select('id', 'name', 'icon')
+                ->get();
+            //  dd($data);
             return DataTables::of($data)
                 ->addColumn('action', function ($row) {
-                   // dd($row);
+                    // dd($row);
                     $editUrl = route('ProductCategory.edit', $row->id);
                     $deleteUrl = route('ProductCategory.destroy', $row->id);
                     $action = '<a href="' . $editUrl . '" class="btn btn-primary btn-sm"><i class="fa-solid fa-pen-to-square"></i></a>&nbsp'
-                   
+
                         . '<button data-href="' . $deleteUrl . '" class="btn btn-sm btn-danger delete_button"><i class="fa-solid fa-trash-can"></i></button>';
 
                     return $action;
@@ -50,55 +50,55 @@ class ProductCategoryController extends Controller
         return view('dashboard.productcategory.create');
     }
 
- 
+
     public function store(Request $request)
     {
         $validatedData = $request->validate([
             'name' => 'required|string|max:255',
             'icon' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
-    
+
         $iconPath = $request->file('icon')->store('icons', 'public');
-    
+
         $productcategory = new ProductCategory();
         $productcategory->name = $validatedData['name'];
         $productcategory->icon = $iconPath;
         $productcategory->save();
-    
+
         return redirect()->route('ProductCategory.index')->with('success', 'Category added successfully.');
     }
-     // Edit view category data
+    // Edit view category data
     public function edit($id)
     {
         $productcategory = ProductCategory::findOrFail($id);
-      // dd( $category);
+        // dd( $category);
         return view('dashboard.productcategory.create', compact('productcategory'));
     }
     public function update(Request $request, $id)
     {
         $productcategory = ProductCategory::findOrFail($id);
-    
+
         $validatedData = $request->validate([
             'name' => 'required|string|max:255',
             'icon' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
-    
+
         if ($request->hasFile('icon')) {
             $iconPath = $request->file('icon')->store('icons', 'public');
             $productcategory->icon = $iconPath;
         }
-    
+
         $productcategory->name = $validatedData['name'];
         $productcategory->save();
-    
+
         return redirect()->route('ProductCategory.index')->with('success', 'Category updated successfully.');
     }
-    
+
     // Delete category
     public function destroy($id)
     {  //  dd($id);
-        $productcategory = ProductCategory::find($id);  
-// dd($category);
+        $productcategory = ProductCategory::find($id);
+        // dd($category);
         if (!$productcategory) {
             return response()->json(['error' => 'Category not found.'], 404);
         }
