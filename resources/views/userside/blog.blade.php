bc09464676b1645c5bf3ca4e578dccb03db8bd4a
Anam042297 <Anam042297@gmail.com>
2024-11-25 00:20:35 +0500

    Update blog.blade.php

--- resources/views/userside/blog.blade.php
+++ resources/views/userside/blog.blade.php
@@ -43,33 +43,9 @@
                                             <span class="visually-hidden">Next</span>
                                         </button>
                                     @endif
-                                </div> --}}
-                              
-
-<div id="imageCarousel{{ $post->id }}" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-        @foreach($post->images as $index => $image)
-            <div class="carousel-item @if($index === 0) active @endif">
-                <img src="{{ asset($image->url) }}" class="d-block w-100" alt="pet image" style="height: 250px; object-fit: scale-down;">
-            </div>
-        @endforeach
-    </div>
-
-    @if($post->images->count() > 1)
-        <!-- Previous button -->
-        <button class="carousel-control-prev" type="button" data-bs-target="#imageCarousel{{ $post->id }}" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        
-        <!-- Next button -->
-        <button class="carousel-control-next" type="button" data-bs-target="#imageCarousel{{ $post->id }}" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    @endif
-</div>
-
+                                </div> --}}  @foreach($product->productimages as $image)
+                <img src="{{ $image->url }}" alt="{{ $product->name }}" style="width: 100px; height: auto;">
+                @endforeach
                                 <a class="blog_item_date">
                                     <h3>{{ $post->created_at->format('d') }}</h3>
                                     <p>{{ $post->created_at->format('M') }}</p>
